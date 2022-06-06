33cf6d43bbea02fba71f872809af14887d64c319
niiyazobug <niiyaz5555@gmail.com>
2022-06-07 03:43:07 +0600

    mongoDB setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -13,12 +14,26 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PSS}@cluster0.a36m2.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 async function run() {
     try {
+        await client.connect()
+        const fileCollection = client.db('neyaz_portfolio_collection').collection('My-File')
 
         app.get('/', (req, res)=>{
             res.download('./Assets/Neyaz Mobalik Nafiz.pdf')
         })
+
+        // app.get('/portfolio', async (req,res)=>{
+        //     const id = req.params.id
+        //     const query = { _id: ObjectId(id) }
+        //     const result = await fileCollection.findOne(query).toArray()
+        //     res.download(result)
+        // })
 
     }
     finally { }
